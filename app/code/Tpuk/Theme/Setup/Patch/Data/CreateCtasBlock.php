3fdbc129b2bafa7a18ec33fdc4a4da52a1ecdde1
karlsminton <ksm1nn0@gmail>
2022-10-06 18:34:58 +0100

    homepage ctas added

---
+++ app/code/Tpuk/Theme/Setup/Patch/Data/CreateCtasBlock.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Tpuk\Theme\Setup\Patch\Data;
+
+use Magento\Cms\Api\BlockRepositoryInterface;
+use Magento\Cms\Api\Data\BlockInterfaceFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Patch is mechanism, that allows to do atomic upgrade data changes
+ */
+class CreateCtasBlock implements DataPatchInterface
+{
+    const BLOCK_IDENTIFIER = 'cta-columns';
+
+    /**
+     * @var ModuleDataSetupInterface $moduleDataSetup
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var BlockInterfaceFactory
+     */
+    private $blockFactory;
+
+    /**
+     * @var BlockRepositoryInterface
+     */
+    private $blockRepository;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param BlockInterfaceFactory $blockFactory
+     * @param BlockRepositoryInterface $blockRepository
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        BlockInterfaceFactory $blockFactory,
+        BlockRepositoryInterface $blockRepository
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->blockFactory = $blockFactory;
+        $this->blockRepository = $blockRepository;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->startSetup();
+        $block = $this->blockFactory->create();
+        $content = file_get_contents(__DIR__ . "/CreateCtasBlock/cta-columns.html");
+        $block->setIdentifier(self::BLOCK_IDENTIFIER)
+            ->setContent($content)
+            ->setIsActive(true)
+            ->setTitle('Homepage CTA Columns');
+
+        $this->blockRepository->save($block);
+        $this->moduleDataSetup->endSetup();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
